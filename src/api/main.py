87ceaf899076c1048a806d0b7da46a9c682f39bb
Pandidharan22 <pandidharan7@gmail.com>
2026-04-17 21:10:59 +0530

    Added logging system with app startup and request-level logging

--- src/api/main.py
+++ src/api/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from src.core.config import Settings, settings
+from src.core.logger import logger
 
 app = FastAPI(
     title=settings.app_name,
     debug=settings.debug,
     version="1.0.0"
 )
-
+@app.on_event("startup")
+def startup_event():
+    logger.info("Application Startup")
+    
 @app.get("/")
 def root():
+    logger.info("Root endpoint called")
     return {"message" : f"{settings.app_name} is running"}
