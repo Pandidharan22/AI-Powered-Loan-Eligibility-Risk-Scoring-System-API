4c3a7567f0732eda4cb390ccac9442add528da1d
Pandidharan22 <pandidharan7@gmail.com>
2026-04-28 12:27:22 +0530

    Improved Input Validation in request.py

--- src/api/schemas/request.py
+++ src/api/schemas/request.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class PredictionRequest(BaseModel):
-    Age: int
-    Income: int
-    LoanAmount: int
-    CreditScore: int
-    MonthsEmployed: int
-    NumCreditLines: int
-    InterestRate: float
-    LoanTerm: int
-    DTIRatio: float
+    Age: int = Field(..., ge=18, le=100)
+    Income: int = Field(..., gt=0)
+    LoanAmount: int = Field(..., gt=0)
+    CreditScore: int = Field(..., ge=300, le=900)
+    MonthsEmployed: int = Field(..., ge=0)
+    NumCreditLines: int = Field(..., ge=1)
+    InterestRate: float = Field(..., gt=0)
+    LoanTerm: int = Field(..., ge=1)
+    DTIRatio: float = Field(..., ge=0, le=1)
+
     Education: str
     EmploymentType: str
     MaritalStatus: str
